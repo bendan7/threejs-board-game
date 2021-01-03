28f306184a19cf5be6c128743beb632077ba5260
Ben Dan <bendan7@gmail.com>
2021-01-03 11:04:32 +0200

    Add board game

--- src/app/engine/engine.service.ts
+++ src/app/engine/engine.service.ts
@@ -1,20 +1,20 @@
-import * as THREE from 'three';
-import {ElementRef, Injectable, NgZone, OnDestroy} from '@angular/core';
+import * as THREE from "three";
+import { ElementRef, Injectable, NgZone, OnDestroy } from "@angular/core";
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class EngineService implements OnDestroy {
   private canvas: HTMLCanvasElement;
   private renderer: THREE.WebGLRenderer;
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
   private light: THREE.AmbientLight;
-
   private cube: THREE.Mesh;
-
   private frameId: number = null;
 
-  public constructor(private ngZone: NgZone) {
-  }
+  private gameBoard: THREE.Group = null;
+  private cubes: Array<THREE.Mesh> = new Array(9);
+
+  public constructor(private ngZone: NgZone) {}
 
   public ngOnDestroy(): void {
     if (this.frameId != null) {
@@ -28,8 +28,8 @@ export class EngineService implements OnDestroy {
 
     this.renderer = new THREE.WebGLRenderer({
       canvas: this.canvas,
-      alpha: true,    // transparent background
-      antialias: true // smooth edges
+      alpha: true, // transparent background
+      antialias: true, // smooth edges
     });
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -37,36 +37,58 @@ export class EngineService implements OnDestroy {
     this.scene = new THREE.Scene();
 
     this.camera = new THREE.PerspectiveCamera(
-      75, window.innerWidth / window.innerHeight, 0.1, 1000
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
     );
+
     this.camera.position.z = 5;
     this.scene.add(this.camera);
 
     // soft white light
     this.light = new THREE.AmbientLight(0x404040);
-    this.light.position.z = 10;
-    this.scene.add(this.light);
+
+    this.gameBoard = new THREE.Group();
+    var cubes = new Array(9);
 
     const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    this.cube = new THREE.Mesh(geometry, material);
-    this.scene.add(this.cube);
 
+    for (var i = 0; i < 9; i++) {
+      const material = new THREE.MeshBasicMaterial({
+        color: this.getRandomColor(),
+      });
+
+      const cube = new THREE.Mesh(geometry, material);
+
+      cube.position.x = i % 3;
+      if (i > 2 && i <= 5) {
+        cube.position.y = 1;
+      }
+      if (i > 5) {
+        cube.position.y = 2;
+      }
+
+      cubes[i] = cube;
+      this.gameBoard.add(cube);
+    }
+
+    this.scene.add(this.gameBoard);
   }
 
   public animate(): void {
     // We have to run this outside angular zones,
     // because it could trigger heavy changeDetection cycles.
     this.ngZone.runOutsideAngular(() => {
-      if (document.readyState !== 'loading') {
+      if (document.readyState !== "loading") {
         this.render();
       } else {
-        window.addEventListener('DOMContentLoaded', () => {
+        window.addEventListener("DOMContentLoaded", () => {
           this.render();
         });
       }
 
-      window.addEventListener('resize', () => {
+      window.addEventListener("resize", () => {
         this.resize();
       });
     });
@@ -77,8 +99,8 @@ export class EngineService implements OnDestroy {
       this.render();
     });
 
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    this.gameBoard.rotation.x += 0.01;
+    this.gameBoard.rotation.y += 0.01;
     this.renderer.render(this.scene, this.camera);
   }
 
@@ -90,5 +112,11 @@ export class EngineService implements OnDestroy {
     this.camera.updateProjectionMatrix();
 
     this.renderer.setSize(width, height);
+  }
+
+  public getRandomColor(): string {
+    return (
+      "#" + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, "0")
+    );
   }
 }
